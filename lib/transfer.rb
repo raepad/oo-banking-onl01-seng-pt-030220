e081dc1b4d308df908d9bcaf1d70643cb71af199
Rachel Padworski <rachelpadworski@gmail.com>
2020-04-03 02:59:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,7 @@ class Transfer
     if @sender.balance > @amount && valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
+      @status == "complete"
     
     #closed account or bad transfer to equal "Transaction rejected. Please check your account balance." and list the transfer.status as "rejected."
     
