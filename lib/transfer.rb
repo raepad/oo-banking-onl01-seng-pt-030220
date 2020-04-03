6f22a964cc923fee89301587cfd8d97cf89230dc
Rachel Padworski <rachelpadworski@gmail.com>
2020-04-03 02:36:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,15 @@
 class Transfer
-  attr_reader :name 
+  attr_accessor :transfer, :status
+  attr_reader :sender, :receiver, :amount
   
   def initialize(name)
-    @name = name 
+    @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
   end
+  
   
   
   
