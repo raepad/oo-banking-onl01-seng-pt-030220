cfe82252caeb2feee33131c463aa19b6315d6e2d
Rachel Padworski <rachelpadworski@gmail.com>
2020-04-03 03:06:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   
   def execute_transaction
     #transfer @amount from @sender to @receiver
-    if @sender.balance > @amount && self.valid?
+    if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
